410641d2b03ca495cd1571da2b839b27c62f3790
Stagiaire un SM. MAYEXPERINFO <stagiaireun@sarl-mayexperinfo.com>
2025-05-27 16:31:58 +0300

    création de add + confirmation de suppression

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+// Configuration de l'app
+
 const express = require('express');
 const mysql = require('mysql2/promise');
 const app = express();
 const PORT = 3000;
 const bcrypt = require('bcrypt');
 
-
+// App EJS
 app.set('view engine', 'ejs');
 
 // Middleware pour les formulaires (utile plus tard pour admin)
@@ -36,7 +38,58 @@ app.get('/horaires', async (req, res) => {
   }
 });
 
+app.get('/admin/delete/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    await db.execute('DELETE FROM horaires WHERE id = ?', [id]);
+    res.redirect('/admin');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur lors de la suppression.');
+  }
+});
+
+app.post('/admin/update/:id', async (req, res) => {
+  const { jour, type, ouverture, fermeture, commentaire } = req.body;
+  const id = parseInt(req.params.id);
+
+  try {
+    await db.execute(`
+      UPDATE horaires 
+      SET jour = ?, type = ?, ouverture = ?, fermeture = ?, commentaire = ?
+      WHERE id = ?
+    `, [jour, type, ouverture || null, fermeture || null, commentaire || null, id]);
+
+    res.redirect('/admin');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erreur lors de la modification.');
+  }
+});
+
+
+// Ecoute du port
 app.listen(PORT, () => {
   console.log(`🟢 Serveur en ligne sur http://localhost:${PORT}`);
 });
 
+// Afficher le formulaire
+app.get('/admin/add', (req, res) => {
+  res.render('add');
+});
+
+// Traiter le formulaire
+app.post('/admin/add', async (req, res) => {
+  const { jour, type, ouverture, fermeture, commentaire } = req.body;
+  try {
+    await db.execute(`
+      INSERT INTO horaires (jour, type, ouverture, fermeture, commentaire)
+      VALUES (?, ?, ?, ?, ?)
+    `, [jour, type, ouverture || null, fermeture || null, commentaire || null]);
+
+    res.redirect('/admin');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Erreur lors de l’ajout.");
+  }
+});
